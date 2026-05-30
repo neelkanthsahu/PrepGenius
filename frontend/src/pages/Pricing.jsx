688767c663c674626030e0d48c37c31d8c1b1184
neelkanthsahu <neelkanthsahu1503@gmail.com>
2026-05-30 17:56:31 +0530

    razorpay setup completed

--- frontend/src/pages/Pricing.jsx
+++ frontend/src/pages/Pricing.jsx
@@ -73,11 +73,12 @@ function Pricing() {
       },{withCredentials:true})
       
 
+
       const options = {
       key: import.meta.env.VITE_RAZORPAY_KEY_ID,
       amount: result.data.amount,
       currency: "INR",
-      name: "InterviewIQ.AI",
+      name: "PrepGenius.AI",
       description: `${plan.name} - ${plan.credits} Credits`,
       order_id: result.data.id,
 
