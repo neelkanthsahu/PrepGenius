51a2f555ea2a4b1f2f7ab5449bb4b783c1c44ca6
NEELKANTH SAHU <105596979+neelkanthsahu@users.noreply.github.com>
2026-06-08 21:06:40 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,10 @@ import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:[
+      "http://localhost:5173",
+      "https://prepgenius-frontend-t03t.onrender.com"
+    ],
     credentials:true
 }))
 
